8d7b48e7bcf89bd119c282ad894bb036f3664979
sergey-kvitka <45879111+sergey-kvitka@users.noreply.github.com>
2023-01-12 23:20:38 +0400

    added offer calculation logic and extracted method SecondaryCalculationService.calculateMonthlyPayment from CreditCalculationServiceImpl

--- src/main/java/com/kvitka/conveyor/services/OfferCalculationService.java
+++ src/main/java/com/kvitka/conveyor/services/OfferCalculationService.java
@@ -2,9 +2,12 @@ package com.kvitka.conveyor.services;
 
 import com.kvitka.conveyor.dtos.LoanApplicationRequestDTO;
 import com.kvitka.conveyor.dtos.LoanOfferDTO;
+import com.kvitka.conveyor.exceptions.PreScoreException;
 
 import java.util.List;
 
 public interface OfferCalculationService {
     List<LoanOfferDTO> calculateOffers(LoanApplicationRequestDTO loanApplicationRequestDTO);
+
+    void preScore(LoanApplicationRequestDTO loanApplicationRequestDTO) throws PreScoreException;
 }
